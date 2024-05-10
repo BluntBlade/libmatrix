c6fad962a4ffb386646392fdfefd83f765ece132
Liang Tao <amethyst.black@gmail.com>
2024-05-10 15:03:58 +0800

    CHANGE: Rearrange function declarations.

--- matrix.h
+++ matrix.h
@@ -37,11 +37,6 @@ extern void mtx_destroy(p_matrix_t mtx);
 extern int mtx_can_do_add(p_matrix_t lhs, p_matrix_t rhs);
 extern int mtx_can_do_multiply(p_matrix_t lhs, p_matrix_t rhs);
 
-inline static int mtx_can_do_sub(p_matrix_t lhs, p_matrix_t rhs)
-{
-    return mtx_can_do_add(lhs, rhs);
-} /* mtx_can_do_sub */
-
 extern int mtx_get_at(p_matrix_t mtx, mtx_count_t row, mtx_count_t col);
 extern void mtx_get_slice_at(p_matrix_t mtx, mtx_count_t row, mtx_count_t col, int ** vals, mtx_count_t * max_cnt);
 
@@ -51,6 +46,16 @@ extern void mtx_set_slice_to(p_matrix_t mtx, mtx_count_t row, mtx_count_t col, i
 extern void mtx_set_from_array(p_matrix_t mtx, int * src_vals[]);
 
 extern p_matrix_t mtx_add_and_store(p_matrix_t mtx, p_matrix_t lhs, p_matrix_t rhs, mtx_option_t opt);
+extern p_matrix_t mtx_sub_and_store(p_matrix_t mtx, p_matrix_t lhs, p_matrix_t rhs, mtx_option_t opt);
+extern p_matrix_t mtx_multiply_and_store(p_matrix_t mtx, p_matrix_t lhs, p_matrix_t rhs, mtx_option_t opt);
+extern p_matrix_t mtx_scalar_multiply_and_store(p_matrix_t mtx, int lhs, p_matrix_t rhs, mtx_option_t opt);
+
+extern p_matrix_t mtx_transpose_and_store(p_matrix_t mtx, p_matrix_t src);
+
+inline static int mtx_can_do_sub(p_matrix_t lhs, p_matrix_t rhs)
+{
+    return mtx_can_do_add(lhs, rhs);
+} /* mtx_can_do_sub */
 
 inline static p_matrix_t mtx_add(p_matrix_t lhs, p_matrix_t rhs, mtx_option_t opt)
 {
@@ -61,8 +66,6 @@ inline static p_matrix_t mtx_add(p_matrix_t lhs, p_matrix_t rhs, mtx_option_t op
     return mtx_add_and_store(mtx, lhs, rhs, opt);
 } /* mtx_add */
 
-extern p_matrix_t mtx_sub_and_store(p_matrix_t mtx, p_matrix_t lhs, p_matrix_t rhs, mtx_option_t opt);
-
 inline static p_matrix_t mtx_sub(p_matrix_t lhs, p_matrix_t rhs, mtx_option_t opt)
 {
     p_matrix_t mtx = mtx_allocate_in_shape_of(lhs);
@@ -71,8 +74,6 @@ inline static p_matrix_t mtx_sub(p_matrix_t lhs, p_matrix_t rhs, mtx_option_t op
     } /* if */
     return mtx_sub_and_store(mtx, lhs, rhs, opt);
 } /* mtx_sub */
-
-extern p_matrix_t mtx_multiply_and_store(p_matrix_t mtx, p_matrix_t lhs, p_matrix_t rhs, mtx_option_t opt);
 
 inline static p_matrix_t mtx_multiply(p_matrix_t lhs, p_matrix_t rhs, mtx_option_t opt)
 {
@@ -83,16 +84,14 @@ inline static p_matrix_t mtx_multiply(p_matrix_t lhs, p_matrix_t rhs, mtx_option
     return mtx_multiply_and_store(mtx, lhs, rhs, opt);
 } /* mtx_multiply */
 
-extern p_matrix_t mtx_scalar_multiply_and_store(p_matrix_t mtx, int lhs, p_matrix_t rhs, mtx_option_t opt);
-
-inline static p_matrix_t mtx_multiply_by_scalar(int lhs, p_matrix_t rhs, mtx_option_t opt)
+inline static p_matrix_t mtx_scalar_multiply(int lhs, p_matrix_t rhs, mtx_option_t opt)
 {
     p_matrix_t mtx = mtx_allocate_in_shape_of(rhs);
     if (! mtx) {
         return NULL;
     } /* if */
     return mtx_scalar_multiply_and_store(mtx, lhs, rhs, opt);
-} /* mtx_multiply_by_scalar */
+} /* mtx_scalar_multiply */
 
 inline static p_matrix_t mtx_zeros(mtx_count_t row_cnt, mtx_count_t col_cnt)
 {
@@ -107,8 +106,6 @@ inline static p_matrix_t mtx_ones(mtx_count_t row_cnt, mtx_count_t col_cnt)
     } /* if */
     return mtx;
 } /* mtx_ones */
-
-extern p_matrix_t mtx_transpose_and_store(p_matrix_t mtx, p_matrix_t src);
 
 inline static p_matrix_t mtx_transpose(p_matrix_t src)
 {

c4d58536b503042db5539586e5c8db98e243bffa
Liang Tao <amethyst.black@gmail.com>
2024-07-29 10:31:36 +0800

    TEST: Add new test cases for mx_v8si_interpolate().

--- test/utils.c
+++ test/utils.c
@@ -167,4 +167,63 @@ Test(Interpolation, mx_v8si_interpolate)
         check_value(y[7], 24);
         check_value(y[8], 17);
     }
+
+    // Case: The number of x is less than 8 and the number of search range is odd.
+    {
+        int32_t xp[] = {3, 6, 9, 12, 15};
+        int32_t fp[] = {3, 6, 9, 12, 15};
+        int32_t x[] = {4, 8, 10, 3, 15, -1, 16};
+        int32_t y[sizeof(x) / sizeof(x[0])];
+
+        mx_v8si_interpolate(y, sizeof(xp) / sizeof(xp[0]), xp, fp, NULL, NULL, NULL, sizeof(x) / sizeof(x[0]), x);
+
+        check_value(y[0], 4);
+        check_value(y[1], 8);
+        check_value(y[2], 10);
+        check_value(y[3], 3);
+        check_value(y[4], 15);
+        check_value(y[5], 3);
+        check_value(y[6], 15);
+    }
+
+    // Case: The number of x is equal to 8 the number of search range is odd.
+    {
+        int32_t xp[] = {1,  5, 10, 12, 17};
+        int32_t fp[] = {3, 17, 35, 42, 59};
+        int32_t x[] = {4, 8, 10, 1, 17, -1, 13, 18};
+        int32_t y[sizeof(x) / sizeof(x[0])];
+
+        mx_v8si_interpolate(y, sizeof(xp) / sizeof(xp[0]), xp, fp, NULL, NULL, NULL, sizeof(x) / sizeof(x[0]), x);
+
+        check_value(y[0], 13);
+        check_value(y[1], 28);
+        check_value(y[2], 35);
+        check_value(y[3], 3);
+        check_value(y[4], 59);
+        check_value(y[5], 3);
+        check_value(y[6], 45);
+        check_value(y[7], 59);
+    }
+
+    // Case: The number of x is greater than 8 the number of search range is odd.
+    {
+        int32_t xp[] = {1,  5, 10, 12, 17};
+        int32_t fp[] = {3, 17, 35, 42, 59};
+        int32_t x[] = {4, 8, 10, 1, 12, -1, 13, 7, 5, 17, 18};
+        int32_t y[sizeof(x) / sizeof(x[0])];
+
+        mx_v8si_interpolate(y, sizeof(xp) / sizeof(xp[0]), xp, fp, NULL, NULL, NULL, sizeof(x) / sizeof(x[0]), x);
+
+        check_value(y[0], 13);
+        check_value(y[1], 28);
+        check_value(y[2], 35);
+        check_value(y[3], 3);
+        check_value(y[4], 42);
+        check_value(y[5], 3);
+        check_value(y[6], 45);
+        check_value(y[7], 24);
+        check_value(y[8], 17);
+        check_value(y[9], 59);
+        check_value(y[10], 59);
+    }
 }

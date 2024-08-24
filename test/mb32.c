f2932631216ab37373fe0150ca63d6b4602d5050
Liang Tao <amethyst.black@gmail.com>
2024-08-24 10:53:18 +0800

    TEST: Add test cases for mb32_chk_locate_by_number().

--- test/mb32.c
+++ test/mb32.c
@@ -611,7 +611,7 @@ Test(PropertyAccess, mb32_padded_bytes)
     }
 }
 
-Test(Utilities, mb32_chk_locate)
+Test(Utilities, mb32_chk_locate_by_number)
 {
     // Case: 1x2 matrix
     {
@@ -619,26 +619,8 @@ Test(Utilities, mb32_chk_locate)
 
         mb32_init(&ms, 1, 2, 4, 8);
 
-        check_non_null(mb32_chk_locate(&ms, 0, 0));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 0, 0));
-
-        check_non_null(mb32_chk_locate(&ms, 0, 1));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 0, 1));
-
-        mb32_clean(&ms);
-    }
-
-    // Case: 1x8 matrix
-    {
-        mb32_stor_t ms;
-
-        mb32_init(&ms, 1, 8, 4, 8);
-
-        check_non_null(mb32_chk_locate(&ms, 0, 0));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 0, 0));
-
-        check_non_null(mb32_chk_locate(&ms, 0, 7));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 0, 7));
+        check_non_null(mb32_chk_locate_by_number(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_number(&ms, 0, 0));
 
         mb32_clean(&ms);
     }
@@ -649,11 +631,11 @@ Test(Utilities, mb32_chk_locate)
 
         mb32_init(&ms, 1, 9, 4, 8);
 
-        check_non_null(mb32_chk_locate(&ms, 0, 0));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 0, 0));
+        check_non_null(mb32_chk_locate_by_number(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_number(&ms, 0, 0));
 
-        check_non_null(mb32_chk_locate(&ms, 0, 8));
-        check_pointer(ms.chks + 1, mb32_chk_locate(&ms, 0, 8));
+        check_non_null(mb32_chk_locate_by_number(&ms, 0, 1));
+        check_pointer(ms.chks + 1, mb32_chk_locate_by_number(&ms, 0, 1));
 
         mb32_clean(&ms);
     }
@@ -664,26 +646,8 @@ Test(Utilities, mb32_chk_locate)
 
         mb32_init(&ms, 2, 1, 4, 8);
 
-        check_non_null(mb32_chk_locate(&ms, 0, 0));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 0, 0));
-
-        check_non_null(mb32_chk_locate(&ms, 1, 0));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 1, 0));
-
-        mb32_clean(&ms);
-    }
-
-    // Case: 8x1 matrix
-    {
-        mb32_stor_t ms;
-
-        mb32_init(&ms, 8, 1, 4, 8);
-
-        check_non_null(mb32_chk_locate(&ms, 0, 0));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 0, 0));
-
-        check_non_null(mb32_chk_locate(&ms, 7, 0));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 7, 0));
+        check_non_null(mb32_chk_locate_by_number(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_number(&ms, 0, 0));
 
         mb32_clean(&ms);
     }
@@ -694,11 +658,11 @@ Test(Utilities, mb32_chk_locate)
 
         mb32_init(&ms, 9, 1, 4, 8);
 
-        check_non_null(mb32_chk_locate(&ms, 0, 0));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 0, 0));
+        check_non_null(mb32_chk_locate_by_number(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_number(&ms, 0, 0));
 
-        check_non_null(mb32_chk_locate(&ms, 8, 0));
-        check_pointer(ms.chks + 1, mb32_chk_locate(&ms, 8, 0));
+        check_non_null(mb32_chk_locate_by_number(&ms, 1, 0));
+        check_pointer(ms.chks + 1, mb32_chk_locate_by_number(&ms, 1, 0));
 
         mb32_clean(&ms);
     }
@@ -709,11 +673,8 @@ Test(Utilities, mb32_chk_locate)
 
         mb32_init(&ms, 8, 8, 4, 8);
 
-        check_non_null(mb32_chk_locate(&ms, 0, 0));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 0, 0));
-
-        check_non_null(mb32_chk_locate(&ms, 7, 7));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 7, 7));
+        check_non_null(mb32_chk_locate_by_number(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_number(&ms, 0, 0));
 
         mb32_clean(&ms);
     }
@@ -724,17 +685,17 @@ Test(Utilities, mb32_chk_locate)
 
         mb32_init(&ms, 9, 9, 4, 8);
 
-        check_non_null(mb32_chk_locate(&ms, 0, 0));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 0, 0));
+        check_non_null(mb32_chk_locate_by_number(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_number(&ms, 0, 0));
 
-        check_non_null(mb32_chk_locate(&ms, 7, 8));
-        check_pointer(ms.chks + 1, mb32_chk_locate(&ms, 7, 8));
+        check_non_null(mb32_chk_locate_by_number(&ms, 0, 0));
+        check_pointer(ms.chks + 1, mb32_chk_locate_by_number(&ms, 0, 1));
 
-        check_non_null(mb32_chk_locate(&ms, 8, 7));
-        check_pointer(ms.chks + 2, mb32_chk_locate(&ms, 8, 7));
+        check_non_null(mb32_chk_locate_by_number(&ms, 1, 0));
+        check_pointer(ms.chks + 2, mb32_chk_locate_by_number(&ms, 1, 0));
 
-        check_non_null(mb32_chk_locate(&ms, 8, 8));
-        check_pointer(ms.chks + 3, mb32_chk_locate(&ms, 8, 8));
+        check_non_null(mb32_chk_locate_by_number(&ms, 1, 1));
+        check_pointer(ms.chks + 3, mb32_chk_locate_by_number(&ms, 1, 1));
 
         mb32_clean(&ms);
     }
@@ -745,17 +706,164 @@ Test(Utilities, mb32_chk_locate)
 
         mb32_init(&ms, 17, 17, 4, 8);
 
-        check_non_null(mb32_chk_locate(&ms, 0, 0));
-        check_pointer(ms.chks, mb32_chk_locate(&ms, 0, 0));
+        check_non_null(mb32_chk_locate_by_number(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_number(&ms, 0, 0));
 
-        check_non_null(mb32_chk_locate(&ms, 8, 8));
-        check_pointer(ms.chks + 4, mb32_chk_locate(&ms, 8, 8));
+        check_non_null(mb32_chk_locate_by_number(&ms, 1, 1));
+        check_pointer(ms.chks + 4, mb32_chk_locate_by_number(&ms, 1, 1));
 
-        check_non_null(mb32_chk_locate(&ms, 15, 15));
-        check_pointer(ms.chks + 4, mb32_chk_locate(&ms, 15, 15));
+        check_non_null(mb32_chk_locate_by_number(&ms, 2, 2));
+        check_pointer(ms.chks + 8, mb32_chk_locate_by_number(&ms, 2, 2));
 
-        check_non_null(mb32_chk_locate(&ms, 16, 16));
-        check_pointer(ms.chks + 8, mb32_chk_locate(&ms, 16, 16));
+        mb32_clean(&ms);
+    }
+}
+
+Test(Utilities, mb32_chk_locate_by_index)
+{
+    // Case: 1x2 matrix
+    {
+        mb32_stor_t ms;
+
+        mb32_init(&ms, 1, 2, 4, 8);
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 0, 0));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 1));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 0, 1));
+
+        mb32_clean(&ms);
+    }
+
+    // Case: 1x8 matrix
+    {
+        mb32_stor_t ms;
+
+        mb32_init(&ms, 1, 8, 4, 8);
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 0, 0));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 7));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 0, 7));
+
+        mb32_clean(&ms);
+    }
+
+    // Case: 1x9 matrix
+    {
+        mb32_stor_t ms;
+
+        mb32_init(&ms, 1, 9, 4, 8);
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 0, 0));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 8));
+        check_pointer(ms.chks + 1, mb32_chk_locate_by_index(&ms, 0, 8));
+
+        mb32_clean(&ms);
+    }
+
+    // Case: 2x1 matrix
+    {
+        mb32_stor_t ms;
+
+        mb32_init(&ms, 2, 1, 4, 8);
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 0, 0));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 1, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 1, 0));
+
+        mb32_clean(&ms);
+    }
+
+    // Case: 8x1 matrix
+    {
+        mb32_stor_t ms;
+
+        mb32_init(&ms, 8, 1, 4, 8);
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 0, 0));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 7, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 7, 0));
+
+        mb32_clean(&ms);
+    }
+
+    // Case: 9x1 matrix
+    {
+        mb32_stor_t ms;
+
+        mb32_init(&ms, 9, 1, 4, 8);
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 0, 0));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 8, 0));
+        check_pointer(ms.chks + 1, mb32_chk_locate_by_index(&ms, 8, 0));
+
+        mb32_clean(&ms);
+    }
+
+    // Case: 8x8 matrix
+    {
+        mb32_stor_t ms;
+
+        mb32_init(&ms, 8, 8, 4, 8);
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 0, 0));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 7, 7));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 7, 7));
+
+        mb32_clean(&ms);
+    }
+
+    // Case: 9x9 matrix
+    {
+        mb32_stor_t ms;
+
+        mb32_init(&ms, 9, 9, 4, 8);
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 0, 0));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 7, 8));
+        check_pointer(ms.chks + 1, mb32_chk_locate_by_index(&ms, 7, 8));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 8, 7));
+        check_pointer(ms.chks + 2, mb32_chk_locate_by_index(&ms, 8, 7));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 8, 8));
+        check_pointer(ms.chks + 3, mb32_chk_locate_by_index(&ms, 8, 8));
+
+        mb32_clean(&ms);
+    }
+
+    // Case: 17x17 matrix
+    {
+        mb32_stor_t ms;
+
+        mb32_init(&ms, 17, 17, 4, 8);
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 0, 0));
+        check_pointer(ms.chks, mb32_chk_locate_by_index(&ms, 0, 0));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 8, 8));
+        check_pointer(ms.chks + 4, mb32_chk_locate_by_index(&ms, 8, 8));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 15, 15));
+        check_pointer(ms.chks + 4, mb32_chk_locate_by_index(&ms, 15, 15));
+
+        check_non_null(mb32_chk_locate_by_index(&ms, 16, 16));
+        check_pointer(ms.chks + 8, mb32_chk_locate_by_index(&ms, 16, 16));
 
         mb32_clean(&ms);
     }
